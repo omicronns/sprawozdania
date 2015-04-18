92ff5d4a9643c2d9c56e1b856f8c33fc2592840c
omicronns <omicronns@gmail.com>
2015-04-18 15:59:32 +0200

    mod

--- TS/SEM6/TS_LAB3/checkLap2.m
+++ TS/SEM6/TS_LAB3/checkLap2.m
@@ -1,4 +1,4 @@
-function [ ok att, natt ] = checkLap2( eps, v, vgrad, rs )
+function [ ok, l, att, natt, nst ] = checkLap2( eps, v, vgrad, rs )
 %CHECKLAP Summary of this function goes here
 %   Detailed explanation goes here
     step    = 0.1;
@@ -6,23 +6,34 @@ function [ ok att, natt ] = checkLap2( eps, v, vgrad, rs )
     space   = [0:step:eps];
     space   = [space -space(2:length(space))];
     xk      = [0 0];
+    l       = inf;
+    att     = [];
     natt    = [];
+    nst     = [];
     if(v(xk) == 0)
-        att = [0 0];
+        atttmp = [0 0];
         for ii=1:length(space)
             for iii=1:length(space)
                 xk = [space(ii) space(iii)];
                 if(norm(xk) ~= 0)
-                    if(v(xk) <= 0)
+                    if(v(xk) <= 0 || (vgrad(xk)')*rs(xk) >= 0)
                         ok = false;
-                        natt = [natt; xk];
-                    elseif((vgrad(xk)')*rs(xk) >= 0)
-                        ok = false;
-                        natt = [natt; xk];
+                        nst = [nst; xk];
+                        ltmp = v(xk);
+                        if(ltmp < l)
+                            l = ltmp;
+                        end
                     else
-                        att = [att; xk];
+                        atttmp = [atttmp; xk];
                     end
                 end
+            end
+        end
+        for ii=1:length(atttmp)
+            if(v(atttmp(ii,:)) > l)
+                natt = [natt; atttmp(ii,:)];
+            else
+                att = [att; atttmp(ii,:)];
             end
         end
     else
