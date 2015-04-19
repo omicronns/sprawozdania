37a58790e4e90127311a31ee316138386f741f62
omicronns <omicronns@gmail.com>
2015-04-19 22:20:02 +0200

    ts lab4 update

--- TS/SEM6/TS_LAB4/runme.m
+++ TS/SEM6/TS_LAB4/runme.m
@@ -1,7 +1,8 @@
 %% Równanie stanu sequ1, funkcjonał v11
-[l, att, natt, nst, ndn] = checkLap2(8, @(x) true, @v11, @v11grad, @sequ1);
+[l1, att, natt, nst, ndn] = checkLap2(8, @(x) true, @v11, @v11grad, @sequ1);
 figure
 axis([-8 8 -8 8])
+title('System1 v11')
 hold on
 if(~isempty(att))
     plot(att(:,1), att(:,2),'g.')
@@ -22,10 +23,11 @@ for ii=1:5
 end
 
 %% Równanie stanu sequ1, funkcjonał v12
-[l, att, natt, nst, ndn] = ...
+[l2, att, natt, nst, ndn] = ...
     checkLap2(8, @(x) x(1)*x(2) < 1, @v12, @v12grad, @sequ1);
 figure
 axis([-8 8 -8 8])
+title('System1 v12')
 hold on
 if(~isempty(att))
     plot(att(:,1), att(:,2),'g.')
@@ -46,9 +48,10 @@ for ii=1:5
 end
 
 %% Równanie stanu sequ2, funkcjonał v21
-[l, att, natt, nst, ndn] = checkLap2(8, @(x) true, @v21, @v21grad, @sequ2);
+[l3, att, natt, nst, ndn] = checkLap2(8, @(x) true, @v21, @v21grad, @sequ2);
 figure
 axis([-8 8 -8 8])
+title('System2 v21')
 hold on
 if(~isempty(att))
     plot(att(:,1), att(:,2),'g.')
